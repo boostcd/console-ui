a7a99add28fc5a453d8bcd3fd9c17a005c49d066
Iliyan Yotov <iliyan.yotov@gmail.com>
2020-04-01 16:16:19 +0300

    feat: Adding an icon when a project is terminating, actions are disabled; Updating the examples with the new fields

--- src/views/Projects/Projects.styled.js
+++ src/views/Projects/Projects.styled.js
@@ -1,7 +1,15 @@
+import { ExclamationCircle } from '@styled-icons/fa-solid';
 import styled from 'styled-components';
 
 export const TableActions = styled.div`
   > * {
     margin-left: 2px;
   }
+`;
+
+export const TerminatingIcon = styled(ExclamationCircle)`
+  color: orange;
+  width: 1rem;
+  height: 1rem;
+  margin-left: 0.5rem;
 `;
