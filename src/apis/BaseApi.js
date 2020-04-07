bb64aed1e69f03fc0c660714f6438cae628ffe82
Iliyan Yotov <iliyan.yotov@gmail.com>
2020-04-07 11:38:13 +0300

    fix: Throw an error if the response content-type header is not application/json

--- src/apis/BaseApi.js
+++ src/apis/BaseApi.js
@@ -26,7 +26,16 @@ class BaseApi {
     try {
       const response = await this.instance.request(config);
       // console.info(`${config.method} request to ${config.url} responded with: `, response);
-      return response.data;
+
+      // Throw an error if the Content-Type header is not JSON
+      const { data, headers } = response;
+      const { 'content-type': contentType } = headers;
+
+      if (contentType !== 'application/json') {
+        throw new RequestError('Content-Type header is not application/json!');
+      }
+
+      return data;
     } catch (error) {
       const message = `${config.method} request to ${config.url} failed with: ${error.message}`;
 
