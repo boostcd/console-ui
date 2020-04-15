57dde9a24b6b225b65b227d58cb6329e5491f886
Iliyan Yotov <iliyan.yotov@gmail.com>
2020-04-16 00:22:33 +0300

    fix: Final adjustment to the promote status enum

--- src/types/feature.js
+++ src/types/feature.js
@@ -7,7 +7,11 @@ export default {
   title: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
-  promoteStatus: PropTypes.oneOf([PROMOTE_STATUS.PROMOTED, PROMOTE_STATUS.PARTIALLY_PROMOTED]),
+  promoteStatus: PropTypes.oneOf([
+    PROMOTE_STATUS.NOT_PROMOTED,
+    PROMOTE_STATUS.PARTIALLY_PROMOTED,
+    PROMOTE_STATUS.FULLY_PROMOTED,
+  ]),
   // Optional
   waitingSince: PropTypes.string,
   description: PropTypes.string,
